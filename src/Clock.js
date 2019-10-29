31dfda2da4862f84180262828ee57b665cf4837a
PushBtnsDev <kyledchenoweth@gmail.com>
2019-10-29 16:07:56 +1100

    updated something

--- src/Clock.js
+++ src/Clock.js
@@ -1,29 +1,53 @@
 import React from 'react';
+import Dashboard from './Dashboard'
 
 class Clock extends React.Component {
 
   state = {
-    date: new Date()
+    date: new Date(),
+    dashboard: null
   }
 
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      console.log('here')
+      this.setState((prevState, props) => {
+        return { date: new Date() };
+      });
+    }, 1000);
+  }
 
-  // setInterval(() => {
-  //   this.setState((prevState, props) => {
-  //       return { value: prevState.value + 1 };
-  //   });
-  // }, 1000);
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevState)
+    if (prevState.dashboard) {
+      clearInterval(this.intervalId)      
+    }
+  }
+
+  goToDashboard = () => {
+    this.setState({
+      dashboard: true
+    })
+  }
+
   render() {
     const seconds = this.state.date.getSeconds();
     const minutes = this.state.date.getMinutes();
     const hours = this.state.date.getHours();
-    return (
-      <>
-        <h3>Timezone: {this.props.timezone}</h3>
-        <h5>{this.props.date.toLocaleString()}</h5>
-        <h6>{hours} : {minutes} : {seconds}</h6>
-        <img src={this.props.icon} alt="sun or snow" height="500" width="500"></img>
-      </>
-    )
+    const { dashboard } = this.state
+    if (!dashboard) {
+      return (
+        <>
+          <h3>Timezone: {this.props.timezone}</h3>
+          <h5>{this.props.date.toLocaleString()}</h5>
+          <h6>{hours} : {minutes} : {seconds}</h6>
+          <img src={this.props.icon} alt="sun or snow" height="500" width="500"></img>
+          <button onClick={this.goToDashboard}>Go to dashboard</button>
+        </>
+      )
+    } else {
+      return <Dashboard />
+    }
   }
 }
 
